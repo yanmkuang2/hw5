82797e955944b0fa789b5305c27eb4d881987b3b
Yanming Kuang <30281387+yanmkuang2@users.noreply.github.com>
2022-10-28 00:23:44 -0400

    benchmark

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,26 +1,21 @@
 library(microbenchmark)
 
-# get the data
-data(french_fries, package = 'reshape2')
-french_fries <- na.omit(french_fries)
-
-# input data
-x <- french_fries$potato
-y <- french_fries$buttery
-
-# space along which to smooth
-z <- seq(0, 15, length.out = 100)
-
-# run smoothing
-fits <- llr(z = z, x = x, y = y, omega = 2)
-
-# plot the data and the smoother
-plot(x, y)
-lines(z, fits, col = 'red')
-
+set.seed(827)
+n = 15
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+omega = 1
 
 microbenchmark(
   llr(z = z, x = x, y = y, omega = 2)
 )
 
+microbenchmark(
+  llr1(z = z, x = x, y = y, omega = 2)
+)
+
+microbenchmark(
+  llr2(z = z, x = x, y = y, omega = 2)
+  )
 
